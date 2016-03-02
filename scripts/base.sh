94f88c895c3815463b118e121e05b3c6b6ea4c07
Waldemar Zurowski <wzurowski@rhythmone.com>
2016-03-02 15:51:13 +0000

    Added more kernel boot options
    a workaround for https://github.com/mitchellh/vagrant/issues/6653

--- scripts/base.sh
+++ scripts/base.sh
@@ -5,8 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#http://mirror.rackspace.com/ubuntu#gi' /etc/apt/sources.list
 
 # Update the box
-apt-get -y update >/dev/null
-apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip python-minimal >/dev/null
+apt-get -y update
+apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip python-minimal
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
@@ -15,12 +15,16 @@ echo 'UseDNS no' >> /etc/ssh/sshd_config
 cat <<EOF > /etc/default/grub
 # If you change this file, run 'update-grub' afterwards to update
 # /boot/grub/grub.cfg.
+# For full documentation of the options in this file, see:
+#   info -f grub -n 'Simple configuration'
 
 GRUB_DEFAULT=0
+GRUB_HIDDEN_TIMEOUT=0
+GRUB_HIDDEN_TIMEOUT_QUIET=true
 GRUB_TIMEOUT=0
 GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
-GRUB_CMDLINE_LINUX_DEFAULT="quiet"
-GRUB_CMDLINE_LINUX="debian-installer=en_US"
+GRUB_CMDLINE_LINUX_DEFAULT="console=tty1 console=tty0 net.ifnames=0"
+GRUB_CMDLINE_LINUX=""
 EOF
 
 update-grub
